ad63a17c6f242f86d0d28df293a66ffd073843f5
Rafael Albano <rafaelb.albano@gmail.com>
2021-08-23 21:53:12 -0300

    Criado a interface de repositório.

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using DIO.Series.Interfaces;
+
+
+namespace DIO.Series
+
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualiza(int id, Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exclui(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Serie> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProximoId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie RetornaPorId(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
